f0e21f4155168d3f385a3a32a698df4573e1fdfa
affishh <afrinshaik802@gmail.com>
2025-07-12 20:30:33 +0530

    workflows

--- test.py
+++ test.py
@@ -2,7 +2,7 @@ import unittest
 
 from main import addition
 
-class testmain(unittest.TestCase):
+class TestMain(unittest.TestCase):
     def test_add(self):
-        assert addition(3, 4) == 7
-        assert addition(-1, 1) == 0
+        assert addition(3,4) == 7
+        assert addition(-1,1) == 0
